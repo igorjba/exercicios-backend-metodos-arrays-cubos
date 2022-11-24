2ad4c6d2518a503d8d03daa887d2864d82bd1e3a
igorjba <igorjbahia@gmail.com>
2022-11-24 20:10:21 -0300

    :sparkles: feat: Exercício 05 resolvido.

--- 05/scripts.js
+++ 05/scripts.js
@@ -1,1 +1,52 @@
 const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+
+const operacao = "agendar";
+
+const paciente = "Igor";
+
+
+function processoDeAtendimento(fila, TipoOperacao, nomeDoPaciente) {
+
+
+    if (TipoOperacao === "agendar") {
+
+        agendarPaciente(fila, nomeDoPaciente);
+
+    } else if (TipoOperacao === "atender") {
+
+        atenderPaciente(fila, nomeDoPaciente);
+
+    } else if (TipoOperacao === "cancelar") {
+
+        cancelarPaciente(fila, nomeDoPaciente);
+
+    }
+
+    function agendarPaciente(fila, nomeDoPaciente) {
+
+        fila.splice(fila.length, 0, nomeDoPaciente);
+
+        return console.log(`${fila.join(", ")}`);
+
+    }
+
+    function atenderPaciente(fila) {
+
+        fila.splice(0, 1,);
+
+        return console.log(`${fila.join(", ")}`);
+
+    }
+
+    function cancelarPaciente(fila, nomeDoPaciente) {
+
+        localizacaoDoPaciente = fila.indexOf(nomeDoPaciente);
+
+        fila.splice(0, localizacaoDoPaciente + 1,);
+
+        return console.log(`${fila.join(", ")}`);
+
+    }
+
+}
+processoDeAtendimento(pacientes, operacao, paciente);
