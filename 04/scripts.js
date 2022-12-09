a7478d3a52a1b6cef9a473328dda99c8e370476a
igorjba <igorjbahia@gmail.com>
2022-12-09 11:50:06 -0300

    :recycle: refactor: Exercício refatorado.

--- 04/scripts.js
+++ 04/scripts.js
@@ -1,23 +1,13 @@
 const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
 
-const operacao = "atender";
-
-const paciente = "Igor";
-
-
-function processoDeAtendimento(fila, TipoOperacao, nomeDoPaciente) {
-    if (TipoOperacao === "agendar") {
-
-        fila.splice(fila.length, 0, nomeDoPaciente);
-
-        return console.log(`${fila.join(", ")}`);
-
-    } else if (TipoOperacao === "atender") {
-
-        fila.splice(0, 1,);
-
-        return console.log(`${fila.join(", ")}`);
-
+function processoDeAtendimento(fila, tipoOperacao, nomePaciente) {
+    if (tipoOperacao === "atender") {
+        fila.shift();
     }
+    if (tipoOperacao === "agendar") {
+        fila.push(nomePaciente);
+    }
+
+    console.log(fila.join(", "));
 }
-processoDeAtendimento(pacientes, operacao, paciente);
+processoDeAtendimento(pacientes, "agendar", "Igor");
