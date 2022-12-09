e3125e4d97026f0a65e7a7924bdaf376b9051da4
igorjba <igorjbahia@gmail.com>
2022-12-09 09:20:54 -0300

    :recycle: refactor: Exercício refatorado.

--- 02/scripts.js
+++ 02/scripts.js
@@ -1,17 +1,15 @@
 const nomes = ['Juninho', 'Vidal', 'Guido', 'Dani', 'Ruli', 'Diego'];
 const tamanhoDoGrupo = 4;
 
-function dividirGrupos(nomesDoGrupo, qntDePessoasNoGrupo) {
-    let nomesParte1 = nomesDoGrupo.slice(0, qntDePessoasNoGrupo);
-    let nomesParte2 = nomesDoGrupo.slice(qntDePessoasNoGrupo);
+const dividirGrupos = (lista, tamanho) => {
 
-    nomesParte1 = nomesParte1.join(", ");
-    nomesParte2 = nomesParte2.join(", ");
+    let numeroGrupo = 1;
 
-
-
-    console.log(`Grupo 1: ${nomesParte1}.`);
-    console.log(`Grupo 2: ${nomesParte2}.`);
+    for (let i = 0; i < lista.length; i += tamanho) {
+        const grupo = `Grupo ${numeroGrupo}: ${lista.slice(i, (i + tamanho)).join(", ")}`;
+        console.log(grupo);
+        numeroGrupo++
+    }
 }
 
 dividirGrupos(nomes, tamanhoDoGrupo);
