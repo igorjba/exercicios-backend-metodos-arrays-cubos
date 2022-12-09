04cb887ef96ac8e8425d37a7d8a829becffad9e9
igorjba <igorjbahia@gmail.com>
2022-12-09 12:16:28 -0300

    :recycle: refactor: Exercício refatorado.

--- 05/scripts.js
+++ 05/scripts.js
@@ -1,52 +1,20 @@
 const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
 
-const operacao = "agendar";
-
-const paciente = "Igor";
-
-
-function processoDeAtendimento(fila, TipoOperacao, nomeDoPaciente) {
-
-
-    if (TipoOperacao === "agendar") {
-
-        agendarPaciente(fila, nomeDoPaciente);
-
-    } else if (TipoOperacao === "atender") {
-
-        atenderPaciente(fila, nomeDoPaciente);
-
-    } else if (TipoOperacao === "cancelar") {
-
-        cancelarPaciente(fila, nomeDoPaciente);
-
-    }
-
-    function agendarPaciente(fila, nomeDoPaciente) {
-
-        fila.splice(fila.length, 0, nomeDoPaciente);
-
-        return console.log(`${fila.join(", ")}`);
-
-    }
-
-    function atenderPaciente(fila) {
-
-        fila.splice(0, 1,);
-
-        return console.log(`${fila.join(", ")}`);
-
-    }
-
-    function cancelarPaciente(fila, nomeDoPaciente) {
-
-        localizacaoDoPaciente = fila.indexOf(nomeDoPaciente);
-
-        fila.splice(0, localizacaoDoPaciente + 1,);
-
-        return console.log(`${fila.join(", ")}`);
-
-    }
-
+const agendarPaciente = (lista, nomePaciente) => {
+    lista.push(nomePaciente);
+    console.log(lista.join(", "));
 }
-processoDeAtendimento(pacientes, operacao, paciente);
+
+const atenderPaciente = (lista) => {
+    lista.shift();
+    console.log(lista.join(", "));
+}
+
+const cancelarAtendimento = (lista, nomePaciente) => {
+    lista.splice(lista.indexOf(nomePaciente), 1);
+    console.log(lista.join(", "));
+}
+
+agendarPaciente(pacientes, "Igor");
+atenderPaciente(pacientes);
+cancelarAtendimento(pacientes, "João");
